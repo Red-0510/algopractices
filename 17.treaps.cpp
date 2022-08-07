61466b1b9cf7463b05290da558913e493aff76b6
Red-0510 <jaineshmachhi@gmail.com>
2022-08-07 10:12:01 +0530

    check for vscode linking with github

---
+++ 17.treaps.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+
+#define fastio() iosbase_::sync_with_stdio(false);cin.tie(NULL);
+#define fileio() freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#define ll long long int
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define forn(l,n,i) for(ll i=l;i<n;i++) 
+
+void solve(){
+    
+}
+
+int main(){
+
+    //fastio();
+    fileio();
+    ll t;
+    cin>>t;
+    while(t--){
+        solve();
+        cout<<'\n';
+    }
+    return 0;
+}
